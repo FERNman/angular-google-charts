e38ca60eb13364b341440dba4a442b26ffe8be31
Gabriel Sperrer <gabriel.sperrer@gmail.com>
2018-08-29 08:13:17 +0200

    Implemented dynamic resize in the google-chart component

--- projects/angular-google-charts/src/lib/google-chart/google-chart.component.ts
+++ projects/angular-google-charts/src/lib/google-chart/google-chart.component.ts
@@ -1,6 +1,9 @@
 /// <reference types="google.visualization"/>
 
-import { Component, OnInit, ElementRef, Input, ChangeDetectionStrategy, OnChanges, Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, ElementRef, Input, ChangeDetectionStrategy,
+  OnChanges, Output, EventEmitter, HostListener
+} from '@angular/core';
 import { ChartErrorEvent, ChartEvent } from '../models/events.model';
 import { ScriptLoaderService } from '../script-loader/script-loader.service';
 import { Observable } from 'rxjs';
@@ -33,6 +36,9 @@ export class GoogleChartComponent implements OnInit, OnChanges {
 
   @Input()
   height: number = undefined;
+
+  @Input()
+  dynamicResize = false;
 
   @Input()
   options: any = {};
@@ -88,6 +94,15 @@ export class GoogleChartComponent implements OnInit, OnChanges {
       height: this.height,
       ...this.options
     };
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (this.dynamicResize) {
+      if (this.wrapper) {
+        this.updateChart();
+      }
+    }
   }
 
   public getChartElement(): HTMLElement {

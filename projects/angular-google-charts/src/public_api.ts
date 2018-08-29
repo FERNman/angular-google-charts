6159fb73b228422eef6fe205454636ba7f355435
Gabriel Sperrer <gabriel.sperrer@gmail.com>
2018-08-29 07:16:06 +0200

    Exposed ScriptLoaderService Added ScriptLoaderService to public_api.ts so that custom components could use it

--- projects/angular-google-charts/src/public_api.ts
+++ projects/angular-google-charts/src/public_api.ts
@@ -5,3 +5,4 @@
 export { GoogleChartComponent } from './lib/google-chart/google-chart.component';
 export { ChartEvent, ChartErrorEvent } from './lib/models/events.model';
 export { GoogleChartsModule } from './lib/google-charts.module';
+export { ScriptLoaderService } from './lib/script-loader/script-loader.service';

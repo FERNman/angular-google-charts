1f3213df46198bb3f9052bdb755ff64915c4a9a4
Gabriel Sperrer <gabriel.sperrer@gmail.com>
2018-10-19 08:29:46 +0200

    Added support for a google api key The GoogleChartsModule can now be created with a forRoot method

--- projects/angular-google-charts/src/lib/google-charts.module.ts
+++ projects/angular-google-charts/src/lib/google-charts.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider, LOCALE_ID, InjectionToken, ModuleWithProviders } from '@angular/core';
 
 import { ScriptLoaderService } from './script-loader/script-loader.service';
 import { GoogleChartComponent } from './google-chart/google-chart.component';
+
+export const GOOGLE_API_KEY = new InjectionToken<string>('GOOGLE_API_KEY');
+
+export const GOOGLE_CHARTS_PROVIDERS: Provider[] = [
+  {
+    provide: ScriptLoaderService,
+    useFactory: setupScriptLoaderService,
+    deps: [
+      LOCALE_ID, GOOGLE_API_KEY
+    ]
+  }
+];
 
 @NgModule({
   providers: [
@@ -14,4 +26,18 @@ import { GoogleChartComponent } from './google-chart/google-chart.component';
     GoogleChartComponent
   ]
 })
-export class GoogleChartsModule { }
+export class GoogleChartsModule {
+  public static forRoot(googleApiKey?: string): ModuleWithProviders<GoogleChartsModule> {
+    return {
+      ngModule: GoogleChartsModule,
+      providers: [
+        GOOGLE_CHARTS_PROVIDERS,
+        { provide: GOOGLE_API_KEY, useValue: googleApiKey ? googleApiKey : '' }
+      ]
+    };
+  }
+}
+
+function setupScriptLoaderService(localeId: string, googleApiKey: string): ScriptLoaderService {
+  return new ScriptLoaderService(localeId, googleApiKey);
+}

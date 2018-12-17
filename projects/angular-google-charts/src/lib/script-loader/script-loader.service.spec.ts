0643831b1d8fa4bb4b7cb22e4e40812496e5ca21
Gabriel Sperrer <gabriel.sperrer@gmail.com>
2018-12-17 20:46:35 +0100

    Added some unit tests

--- projects/angular-google-charts/src/lib/script-loader/script-loader.service.spec.ts
+++ projects/angular-google-charts/src/lib/script-loader/script-loader.service.spec.ts
@@ -19,76 +19,95 @@ describe('ScriptLoaderService', () => {
   });
 
   it('should create a script object', () => {
-    const scripts = Array.from(document.getElementsByTagName('script'));
-    const googleLoaderScript = scripts.find(
-      script => script.src === 'https://www.gstatic.com/charts/loader.js' && script.type === 'text/javascript');
+    const googleLoaderScript = getGoogleLoaderScript();
     expect(googleLoaderScript).toBeTruthy();
   });
 
-  it('should successfully load the google charts package', async(() => {
-    service.onReady.subscribe(() => {
-      expect(google.charts).not.toBeUndefined();
-    });
-  }));
+  describe('script loading tests', () => {
+    // TODO: Refactor these so to really check stuff
 
-  it('#doneLoading should match whether google is already loaded', () => {
-    if (typeof(google) === 'undefined') {
-      expect(service.doneLoading).toBeFalsy();
-    } else {
-      expect(service.doneLoading).toBeTruthy();
+    it('#doneLoading should be false before the script is loaded', () => {
+      if (typeof(google) === 'undefined') {
+        expect(service.doneLoading).toBeFalsy();
+      }
+    });
+
+    it('#doneLoading should be false if another google package has already loaded, but GoogleCharts is not loaded', () => {
+      if (typeof(google.charts) === 'undefined') {
+        expect(service.doneLoading).toBeFalsy();
+      }
+    });
+
+    it('should successfully load the google charts script', async(() => {
+      service.onReady.subscribe(() => {
+        expect(google.charts).not.toBeUndefined();
+        expect(service.doneLoading).toBeTruthy();
+      });
+    }));
+  });
+
+  describe('package loading tests', () => {
+    it('#loadPackages should load the passed packages', async(() => {
+      service.onReady.subscribe(() => {
+        service.loadChartPackages(['corechart', 'bar']).subscribe(() => {
+
+          const loadedCharts = [
+            'AreaChart',
+            'BarChart',
+            'BubbleChart',
+            'CandlestickChart',
+            'ColumnChart',
+            'ComboChart',
+            'PieChart',
+            'Histogram',
+            'LineChart',
+            'ScatterChart',
+            'SteppedAreaChart',
+          ];
+
+          loadedCharts.forEach(chart => {
+            expect(hasKey(google.visualization, chart)).toBeTruthy('Didn\'t load ' + chart);
+          });
+
+          expect(hasKey(google.charts, 'Bar')).toBeTruthy('Didn\'t load Material Bar Chart');
+        });
+      });
+    }));
+
+    it('#loadPackages should be callable twice', async(() => {
+      service.onReady.subscribe(() => {
+        service.loadChartPackages(['treemap']).subscribe(() => {
+          expect(hasKey(google.visualization, 'TreeMap')).toBeTruthy();
+        });
+
+        service.loadChartPackages(['table']).subscribe(() => {
+          expect(hasKey(google.visualization, 'Table')).toBeTruthy();
+        });
+      });
+    }));
+
+    it('after initial doneLoading, onReady should still fire', async(() => {
+      service.onReady.subscribe(() => {
+        expect(service.doneLoading).toBeTruthy();
+
+        service.onReady.subscribe(() => {
+          expect(service.doneLoading).toBeTruthy();
+        });
+      });
+    }));
+
+    function hasKey(obj: object, key: string): boolean {
+      return obj.hasOwnProperty(key);
     }
   });
 
-  it('#loadPackages should load the passed packages', async(() => {
-    service.onReady.subscribe(() => {
-      service.loadChartPackages(['corechart', 'bar']).subscribe(() => {
+  function getGoogleLoaderScript() {
+    const scripts = Array.from(document.getElementsByTagName('script'));
+    const googleLoaderScript = scripts.find(script =>
+      script.src === 'https://www.gstatic.com/charts/loader.js' &&
+      script.type === 'text/javascript');
 
-        const loadedCharts = [
-          'AreaChart',
-          'BarChart',
-          'BubbleChart',
-          'CandlestickChart',
-          'ColumnChart',
-          'ComboChart',
-          'PieChart',
-          'Histogram',
-          'LineChart',
-          'ScatterChart',
-          'SteppedAreaChart',
-        ];
-
-        loadedCharts.forEach(chart => {
-          expect(hasKey(google.visualization, chart)).toBeTruthy('Didn\'t load ' + chart);
-        });
-
-        expect(hasKey(google.charts, 'Bar')).toBeTruthy('Didn\'t load Material Bar Chart');
-      });
-    });
-  }));
-
-  it('#loadPackages should be callable twice', async(() => {
-    service.onReady.subscribe(() => {
-      service.loadChartPackages(['treemap']).subscribe(() => {
-        expect(hasKey(google.visualization, 'TreeMap')).toBeTruthy();
-      });
-
-      service.loadChartPackages(['table']).subscribe(() => {
-        expect(hasKey(google.visualization, 'Table')).toBeTruthy();
-      });
-    });
-  }));
-
-  it('after initial doneLoading, onReady should still fire', async(() => {
-    service.onReady.subscribe(() => {
-      expect(service.doneLoading).toBeTruthy();
-
-      service.onReady.subscribe(() => {
-        expect(service.doneLoading).toBeTruthy();
-      });
-    });
-  }));
-
-  function hasKey(obj: object, key: string): boolean {
-    return obj.hasOwnProperty(key);
+    return googleLoaderScript;
   }
 });
+

9d4face5f6f1da57d79b6d4cb9d0513f1746f866
Joao Ribeiro <joaopribeiro@deloitte.pt>
2019-12-02 15:35:27 +0000

    Refactored GoogleChartsFormatterHelper

--- projects/angular-google-charts/src/lib/helpers/google-chart-formatters.helper.ts
+++ projects/angular-google-charts/src/lib/helpers/google-chart-formatters.helper.ts
@@ -1,24 +1,15 @@
 // @dynamic
 export class GoogleChartsFormatterHelper {
-  private static formatters = {
-    'ArrowFormat': (options) => new google.visualization.ArrowFormat(options),
-    'BarFormat': (options) => new google.visualization.BarFormat(options),
-    'DateFormat': (options) => new google.visualization.DateFormat(options),
-    'NumberFormat': (options) => new google.visualization.NumberFormat(options),
-    'PatternFormat': (options) => new google.visualization.PatternFormat(options),
-    'ColorFormat': () => new google.visualization.ColorFormat()
-  };
-
   public static getFormatter(formatterName: string, options: any): google.visualization.DefaultFormatter {
-    if (Object.keys(this.formatters).indexOf(formatterName) === -1) {
+    if (!google.visualization.hasOwnProperty(formatterName)) {
       throw new Error('No formatter exists with the given name.');
     }
 
     if (formatterName === 'ColorFormat') {
-      return this.setupColorFormatter(this.formatters[formatterName](), options);
+      return this.setupColorFormatter(new google.visualization[formatterName], options);
     }
 
-    return  this.formatters[formatterName](options);
+    return  new google.visualization[formatterName](options);
   }
 
   private static setupColorFormatter(formatter: google.visualization.ColorFormat, options: any): google.visualization.ColorFormat {
